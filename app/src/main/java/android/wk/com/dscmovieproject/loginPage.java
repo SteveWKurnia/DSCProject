7036a2fbde9f62ff165d413e3dd26326d280a260
Stephen William <stevedelta99@yahoo.com>
2019-01-09 13:41:04 +0700

    Trying to fix internet problem

--- app/src/main/java/android/wk/com/dscmovieproject/loginPage.java
+++ app/src/main/java/android/wk/com/dscmovieproject/loginPage.java
@@ -1,11 +1,16 @@
 package android.wk.com.dscmovieproject;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import static android.wk.com.dscmovieproject.HomeFiles.HomeFragment.getFirebaseImageSliderData;
 
@@ -22,11 +27,34 @@ public class loginPage extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(v.getContext(),HomeActivity.class);
-                startActivity(intent);
+                if (isOnline()) {
+                    Intent intent = new Intent(v.getContext(), HomeActivity.class);
+                    startActivity(intent);
+                }
+                else{
+                    Toast.makeText(v.getContext(),"No Internet",Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
         super.onCreate(savedInstanceState);
+    }
+
+    public boolean isOnline() {
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        boolean isWifiConn;
+        boolean isMobileConn;
+        for (Network network : connMgr.getAllNetworks()) {
+            NetworkInfo networkInfo = connMgr.getNetworkInfo(network);
+            if (networkInfo.getType() == ConnectivityManager.TYPE_WIFI) {
+                isWifiConn = networkInfo.isConnected();
+                return isWifiConn;
+            }
+            if (networkInfo.getType() == ConnectivityManager.TYPE_MOBILE) {
+                isMobileConn = networkInfo.isConnected();
+                return isMobileConn;
+            }
+        }
+        return false;
     }
 }
